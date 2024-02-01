6d35b0676e0c472a18f5f4d73e599a1a28778ef1
fengsn <934513774@qq.com>
2024-02-01 17:04:59 +0800

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,52 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import url_for
+
+
+
 
 
 app=Flask(__name__)
-@app.route("/")
-def hello():
-    return"welcome to my watchlist"
+
+
+name = 'fenghaha'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',name=name, movies=movies)
+
+@app.route("/home")
+def hello1():
+    return"主页面"
+
+@app.route('/user/<name>')
+def user_page(name):
+    return name
+
+@app.route('/ha') 
+def text():
+    print(url_for('hello'))
+    return url_for('hello')
+
+
+
+
+
+
+
+
+# if __name__ == "__main__":
+#     app.run()
+    
