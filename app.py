9d69874424ca4905f0be727935050d045d95c31b
fengsn <934513774@qq.com>
2024-02-18 14:25:39 +0800

    add base html template for chapter6

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask import url_for
+from flask import url_for,request, redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
@@ -61,11 +61,53 @@ def initdb(drop):
     db.create_all ()
     click.echo('Initialized database')
 
-@app.route('/')
+#创建电影条目  
+@app.route('/', methods=['GET','POST'])
 def index():
-    # user = User.query.first()
+    if request.method == 'POST':
+        title = request.form.get('title')    # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('Invalid input')
+            return redirect (url_for('index'))  #重定向回主页
+        #保存表单数据到数据库
+        movie = Movie(title=title,year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created')
+        return(url_for('index'))
+        # user = User.query.first()
     movies = Movie.query.all()
     return render_template('index.html', movies=movies)
+    
+
+
+#编辑电影条目
+@app.route('/movie/edit/<int:movie_id>', methods=['GRT','POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) != 4 or len(title) > 60:
+            flash('输入错误')      
+            return redirect(url_for('edit',movie_id= movie_id))   #重定向回对应编辑页面
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('更新成功')
+        return redirect(url_for('index'))
+    return render_template ('edit.html', movie = movie)
+
+#删除电影条目
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('删除成功')
+    return redirect(url_for('index'))
 
 @app.errorhandler(404)
 def page_not_found(e):
